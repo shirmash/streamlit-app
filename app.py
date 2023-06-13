1330d1696e6d09fb1d569d6d34d6f94ed8706224
shirmash <102806894+shirmash@users.noreply.github.com>
2023-06-13 16:25:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -85,16 +85,22 @@ data = pd.read_csv('songs_normalize.csv')
 
 def first_vis(data):
     songs_normalize = data.copy()
-    songs_normalize = songs_normalize.drop(['explicit','genre'], axis=1)
+    songs_normalize = songs_normalize.drop(['explicit', 'genre'], axis=1)
 
     scaler = MinMaxScaler()
-    songs_normalize[songs_normalize.columns.difference(['artist','song', 'year','explicit'])] = scaler.fit_transform(songs_normalize[songs_normalize.columns.difference(['artist','song', 'year','explicit'])])
+    songs_normalize[songs_normalize.columns.difference(['artist', 'song', 'year', 'explicit'])] = scaler.fit_transform(
+        songs_normalize[songs_normalize.columns.difference(['artist', 'song', 'year', 'explicit'])])
 
     # Get the columns names and save only the relevant ones
     column_names = list(songs_normalize.columns.values)
-    features_to_remove = ['song', 'explicit', 'artist', 'year','popularity']
+    features_to_remove = ['song', 'explicit', 'artist', 'year', 'popularity']
     features_names = [item for item in column_names if item not in features_to_remove]
-    avg_popularity = songs_normalize.groupby(['year']).mean().reset_index()
+
+    # Convert non-numeric columns to numeric
+    non_numeric_columns = songs_normalize.select_dtypes(exclude=np.number).columns
+    songs_normalize[non_numeric_columns] = songs_normalize[non_numeric_columns].apply(pd.to_numeric, errors='coerce')
+
+    avg_popularity = songs_normalize.groupby(['year'], as_index=False)[features_names].mean()
 
     # Create the lines for the plot
     lines = []
