c97d97e1104903e702b3ea908bbb626d0b890887
shirmash <102806894+shirmash@users.noreply.github.com>
2023-06-17 22:34:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def first_vis(data):
             lines.append(line)
 
     # Create the layout with checklist dropdown
-      layout = go.Layout(
+     layout = go.Layout(
         title='Average Feature Value per Year',
         title_x=0.3,  # Set the title position to the center
         title_y=0.9,  # Set the title position to the upper part
