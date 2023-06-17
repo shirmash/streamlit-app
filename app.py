d2c229cb0aa222b3f3cfed9214ece5fbf47b5d3c
shirmash <102806894+shirmash@users.noreply.github.com>
2023-06-17 22:14:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def first_vis(data):
         height=600,  # Set the height of the chart
     )
     # Display the figure
-    col1, col2 = st.columns([1, 7])
+    col1, col2 = st.columns([1, 6])
     with col1:
         st.write("")
     with col2:
@@ -163,7 +163,7 @@ def second_vis(data):
         height=700,
     )
     # Display the graph using st.plotly_chart
-    col1, col2 = st.columns([1, 7])
+    col1, col2 = st.columns([1, 6])
     with col1:
         st.write("")
     with col2:
