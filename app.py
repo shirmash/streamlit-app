282bdf30720e2cd7a3c2328b8078d38582f52ef1
shirmash <102806894+shirmash@users.noreply.github.com>
2023-06-20 16:14:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def first_vis(data):
     songs_popular[features_names] = scaler.fit_transform(songs_popular[features_names])
     
     avg_popularity = songs_popular.groupby(['year'], as_index=False)[features_names].mean()
-    avg_popularity[[features_names]]=avg_popularity[[features_names]].round(2)
+    avg_popularity[features_names]=avg_popularity[features_names].round(2)
     # Create the lines for the plot
     lines = []
     for column in avg_popularity.columns:
