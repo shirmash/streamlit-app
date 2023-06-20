1b79f461c5acdb972425a8bf444ad7ad47920f11
shirmash <102806894+shirmash@users.noreply.github.com>
2023-06-20 15:37:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def third_vis(data):
                 x=avg_popularity_genre.index,
                 y=avg_popularity_genre[column],
                 name=column,
-               marker=dict(color='orange', line=dict(color='black', width=1)) )
+               marker=dict(color='orange' )
             bars.append(bar)
             genres.append(column)
 
